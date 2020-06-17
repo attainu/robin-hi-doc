8eed6a84803cdb15b2b464f5caf58d0c9dfe8884
salim mahat <salimmahat22@gmail.com>
2020-06-17 17:45:09 +0530

    added some user schema validation

--- doc/model/User.js
+++ doc/model/User.js
@@ -4,17 +4,23 @@ const UserSchema = new mongoose.Schema({
 
     username: {
         type: String,
-        required: true
+        required: true,
+        max:255,
+        min: 6
     },
 
     email: {
         type: String,
-        required: true
+        required: true,
+        max:255,
+        min: 6
     },
 
     password: {
         type: String,
-        required: true
+        required: true,
+        max:1024,
+        min:6
     },
 
     Profile_createdAt: {
